0e72ede422547a16f6762ea6d839f2c00a038e3c
KeyboardCowboy <chris.albrecht@lullabot.com>
2023-04-10 15:07:07 -0600

    Remove button reference.

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from './Button';
 import './header.css';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
@@ -32,12 +31,12 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
           </>
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <span className="welcome">
+              Can't log in! We removed your buttons!
+            </span>
           </>
         )}
       </div>
